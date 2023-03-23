08a1243eccae89aebed7d6bf398a352ac7036f1c
iosmobileteam1 <iosmobileteam1@gmail.com>
2023-03-23 09:20:52 +0800

    Second

---
+++ CustomButtonLibrary/CustomButtonLibrary/CustomButtonLibrary.h
@@ -0,0 +1,18 @@
+//
+//  CustomButtonLibrary.h
+//  CustomButtonLibrary
+//
+//  Created by HDB ISG on 23/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomButtonLibrary.
+FOUNDATION_EXPORT double CustomButtonLibraryVersionNumber;
+
+//! Project version string for CustomButtonLibrary.
+FOUNDATION_EXPORT const unsigned char CustomButtonLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomButtonLibrary/PublicHeader.h>
+
+
